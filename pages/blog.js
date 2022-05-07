192d25cc4e769c1e25e083c559f4fe8fd41d1b12
Brian <brianfsosa@gmail.com>
2022-05-07 17:42:08 -0300

    api

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,18 @@
+import { useEffect } from 'react'
 import Layout from '../components/Layout'
 
 function Blog() {
+
+   useEffect(() => {
+      const consultarAPI = async () => {
+         const url = 'http://localhost:1337/blogs'
+         const respuesta = await fetch(url)
+         const resultado = await respuesta.json()
+         console.log(resultado)
+      }
+      consultarAPI()
+      
+   },[])
   return (
      <Layout pagina='Blog'>
         <h1>Blog</h1>
