d2e7dea7100fa3a25076ee697380573b68da5b45
Brian <brianfsosa@gmail.com>
2022-05-18 13:29:42 -0300

    commit

--- pages/tienda.js
+++ pages/tienda.js
@@ -3,9 +3,22 @@ import Layout from '../components/Layout'
 function Tienda() {
   return (
      <Layout pagina={'Tienda Virtual'}>
-        <h1>Tienda</h1>
+        <main className='contenedor'>
+            <h1 className='heading'>Nuestra tienda</h1>
+        </main>
      </Layout>
   )
+}
+
+export async function getServerSideProps(){
+   const url = `{process.env.API_URL}/guitarras`
+   const respuesta = await fetch(url)
+   const guitarras = await respuesta.json()
+   return {
+      props: {
+         
+      }
+   }
 }
 
 export default Tienda
